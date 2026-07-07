f5edac18b9bfa030575d4e877603bbaef0eeed00
mohamedlatreche-creator <mohamedlatreche@mizaniyapay.com>
2026-07-07 14:47:51 +0100

    fix: collapse normal flow elements height to 0 and restore position fixed on ticket area for single sheet printing

--- src/components/ProductDetailModal.tsx
+++ src/components/ProductDetailModal.tsx
@@ -234,9 +234,9 @@ export const ProductDetailModal: React.FC<ProductDetailModalProps> = ({
       <div className="ticket-print-area hidden print:flex">
         <div style={{ 
           width: '50mm', 
-          height: '29mm', 
+          height: '30mm', 
           maxWidth: '50mm',
-          maxHeight: '29mm',
+          maxHeight: '30mm',
           display: 'flex', 
           flexDirection: 'column', 
           alignItems: 'center', 
